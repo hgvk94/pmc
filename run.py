46e13a7221268a875a237f4e237d74f6e383a840
Hari Govind V K <hgvk94@gmail.com>
2018-12-08 11:50:23 -0500

    add getopt

--- run.py
+++ run.py
@@ -34,6 +34,7 @@ import semantics as sem
 import scipy.stats
 import numpy
 import sys
+import getopt
 class PD ():
 	def __init__ (self,nm ,p1=None,p2=None):
 		#pd_obj can only be norm for now. change depending on nm
@@ -44,7 +45,7 @@ class PD ():
 	def partition(self,k):
 		delta=1/float(k)
 		return [self.pd_obj.ppf(delta*float(x)) for x in xrange(1,k+1)]
-def model_check_labelled_prog(rel_decl,vars_decl,labelled_ast,replacement_func,toggle):
+def model_check_labelled_prog(rel_decl,vars_decl,labelled_ast,replacement_func,toggle,print_msg):
 	#initialize fixed point engine
 	s=Fixedpoint()
 	s.set(engine="spacer")
@@ -63,30 +64,44 @@ def model_check_labelled_prog(rel_decl,vars_decl,labelled_ast,replacement_func,t
 	pv = vccVisitor.VCGenVisitor (out=st,func_repl=replacement_func,reverse_cond=toggle)
 	pv.visit (labelled_ast)
 	st=pv.out
+	print_msg("Z3 Query for "+str(toggle)+"\n"+st,3)
 	#pass relations and variables and rules to the solver
 	s.parse_string(st)
 	#query error
 	return s.query(err)
-
+def parse_args():
+	opts,args=getopt.getopt(sys.argv,"v:s")
+	verb=0
+	show=False
+	for opt,arg in opts:
+		if opt=='-v':
+			verb=int(arg)
+		if opt=='-s':
+			show=True
+	return verb,show
 def main ():
+	verb,shPlot=parse_args()
 	start=time.time()
 	ini_ast= ast.parse_file (sys.argv [1])
 	agg_prob_safe=-1
 	agg_prob_unsafe=-1
+	print_msg = lambda t,v: (verb >=v and print(t))
 	#initial run of the visitor. Label each node, figure out prob distibutions and error prob
 	lv=labelVisitor.LabelVisitor()
 	lb_ast=lv.visit(ini_ast,createLabel=True)
-	print(lb_ast)
+	print_msg("Labelled AST \n" +str(lb_ast),3)
 	#no random variables. Either the property holds or does not.
 	if(not lv.pd):
-		print("no random variables, treating all variables as non deterministic")
-		res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,[],False)
+		print_msg("no random variables, treating all variables as non deterministic",1)
+		res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,[],False,print_msg)
 		if res==unsat:
-			print("Safe with probability 1")
+			print_msg("Safe with probability 1",0)
 		elif res==sat:
-			print("Unsafe with unknown probability")
+			print_msg("Unsafe with unknown probability",0)
 		else:
-			print("unknown")
+			print_msg("unknown",0)
+		end=time.time()
+		print_msg("Time taken " + str(end-start),0)
 		return 0
 	#collect error prob
 	error_prob = lv.prob
@@ -94,7 +109,7 @@ def main ():
 	assert(error_prob>0)
 	k=1
 	while agg_prob_safe<error_prob and agg_prob_unsafe<(1-error_prob):
-		print("trying " + str(k) + " partitions")
+		print_msg("trying " + str(k) + " partitions",2)
 		agg_prob_safe=0
 		agg_prob_unsafe=0
 		#TODO handle cases when we have more than one random variable
@@ -127,45 +142,45 @@ def main ():
 				ubp=ub
 			else:
 				ubp=100000
-			print("trying bounds "+str(lbp)+" "+str(ubp))
+			print_msg("trying bounds "+str(lbp)+" "+str(ubp),2)
 			#compute lb and ub
 			lv.pd[0].add_bounds(lb,ub)
 			new_func.append(lv.pd[0])
 			#check the complement of the property
 			toggle=True
 			#can use update_rule to make these incremental
-			res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,new_func,toggle)
+			res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,new_func,toggle,print_msg)
 			#unsat is defined as an element of checkSatResult in z3
 			if res==unsat:
 				agg_prob_unsafe=agg_prob_unsafe+1/float(k)
-				print("Unsafe with probability " + str(agg_prob_unsafe))
+				print_msg("Unsafe with probability " + str(agg_prob_unsafe),2)
 				for x in numpy.arange(lbp,ubp,(float(ubp)-float(lbp))/100):
 					red_points.append(x)
 				prop_valid=False
 			if(agg_prob_unsafe>=(1-error_prob)):
-				print("system is NOT SAFE with the desired propbability")
+				print_msg("system is NOT SAFE with the desired propbability",0)
 				break
 			#check the property
 			toggle=False
 			#can use update_rule to make these incremental
-			res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,new_func,toggle)
+			res=model_check_labelled_prog(lv.get_rel_declr(),lv.get_var_declr(),lb_ast,new_func,toggle,print_msg)
 			#unsat is defined as an element of checkSatResult in z3
 			if  res==unsat:
 				assert(prop_valid)
 				agg_prob_safe=agg_prob_safe+1/float(k)
-				print("Safe with probability " + str(agg_prob_safe))
+				print_msg("Safe with probability " + str(agg_prob_safe),2)
 				for x in numpy.arange(lbp,ubp,(float(ubp)-float(lbp))/100):
 					blue_points.append(x)
 			if(agg_prob_safe>=error_prob):
-				print("system is SAFE with the desired propbability")
+				print_msg("system is SAFE with the desired propbability",0)
 				break
-		if(len(red_points)>0):
+		if(len(red_points)>0 and shPlot):
 			print_bounds(red_points,1/float(k),'red')
-		if(len(blue_points)>0):
+		if(len(blue_points)>0 and shPlot):
 			print_bounds(blue_points,1/float(k),'blue')
 		k=k+1
 	end=time.time()
-	print("Time taken" + str(end-start))
+	print_msg("Time taken " + str(end-start),0)
 def print_bounds(points,height,col):
 	plt.xlabel('X values')
 	plt.ylabel('Probabilities')
